95484c770b32e8871e923cf8a97ab463bf353801
Reza-Keymasi <rezakeymasi1996@gmail.com>
2026-08-19 15:38:02 +0330

    feat: display tags in tags page + implement search for tags

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 import ROUTES from "@/constants/routes";
 import { Badge } from "../ui/badge";
-import { getDeviconClassName } from "@/lib/utils";
+import { cn, getDeviconClassName, getTechDescription } from "@/lib/utils";
 import Image from "next/image";
 
 interface TagCardsProps {
@@ -27,6 +27,7 @@ const TagCard = ({
   onTagRemove,
 }: TagCardsProps) => {
   const iconClassName = getDeviconClassName(name);
+  const iconDescription = getTechDescription(name);
 
   const renderBadge = (
     <>
@@ -66,6 +67,29 @@ const TagCard = ({
       </Link>
     );
   }
+
+  return (
+    <Link href={ROUTES.TAG(_id)} className="shadow-light100_darknone">
+      <article className="sm:w-65 w-full flex flex-col background-light900_dark200 border light-border rounded-2xl px-8 py-10">
+        <div className="flex items-center justify-between gap-3">
+          <div className="w-fit rounded-sm px-5 py-1.5 background-light800_dark400">
+            <p className="paragraph-semibold text-dark300_light900">{name}</p>
+          </div>
+          <i className={cn(iconClassName, "text-2xl")} aria-hidden />
+        </div>
+        <p className="w-full small-regular text-dark500_light700 mt-5 line-clamp-3">
+          {iconDescription}
+        </p>
+
+        <p className="small-medium text-dark400_light500 mt-3.5">
+          <span className="body-semibold primary-text-gradient mr-2.5">
+            {questions}+
+          </span>
+          Questions
+        </p>
+      </article>
+    </Link>
+  );
 };
 
 export default TagCard;
