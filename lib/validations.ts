c3dd1d762da8c67b73ee50a19a06c4c91cf70405
Reza-Keymasi <rezakeymasi1996@gmail.com>
2026-09-01 22:59:49 +0330

    feat: implement voting server actions

--- lib/validations.ts
+++ lib/validations.ts
@@ -161,3 +161,15 @@ export const AnswerServerSchema = AnswerSchema.extend({
 export const GetAnswerSchema = PaginatedSearchParamsSchema.extend({
   questionId: z.string().min(1, "Question ID is required"),
 });
+
+export const CreateVoteSchema = z.object({
+  targetId: z.string().min(1, "Target ID is required"),
+  targetType: z.enum(["question", "answer"], {
+    message: "Invalid target type",
+  }),
+  voteType: z.enum(["upvote", "downvote"], { message: "Invalid vote type" }),
+});
+
+export const UpdateVoteCountSchema = CreateVoteSchema.extend({
+  change: z.number().int().min(-1).max(1),
+});
