4aa4a29617734b7a2d0249b82715b0bac82e0393
Reza-Keymasi <rezakeymasi1996@gmail.com>
2026-09-02 18:16:13 +0330

    feat: implement get all users action

--- types/global.d.ts
+++ types/global.d.ts
@@ -68,4 +68,16 @@ declare global {
     upvotes: number;
     downvotes: number;
   }
+
+  interface User {
+    _id: string;
+    name: string;
+    username: string;
+    email: string;
+    bio?: string;
+    image?: string;
+    location?: string;
+    portfolio?: string;
+    reputation?: number;
+  }
 }
