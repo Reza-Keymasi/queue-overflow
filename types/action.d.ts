1066644bc0d68e0ec769786695f0dd5c1db42dff
Reza-Keymasi <rezakeymasi1996@gmail.com>
2026-09-09 04:40:25 +0330

    feat: implement user questions server action

--- types/action.d.ts
+++ types/action.d.ts
@@ -71,3 +71,10 @@ interface CollectionBaseParams {
 interface GetUserParams {
   userId: string;
 }
+
+interface GetUserQuestionsParams extends Omit<
+  PaginatedSearchParams,
+  "quesry" | "filter" | "sort"
+> {
+  userId: string;
+}
